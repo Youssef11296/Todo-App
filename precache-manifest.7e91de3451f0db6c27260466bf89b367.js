167700441d68c88c01987026840b3ddb330626f3
Youssef Shaaban <youssef@gmail.com>
2020-07-16 00:49:40 +0200

    Updates

---
+++ precache-manifest.7e91de3451f0db6c27260466bf89b367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13427c86b4ab877d722c64dfaf9b8271",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "fe9f2cdeecee508c4fd2",
+    "url": "/Todo-App/static/css/main.f0e6e321.chunk.css"
+  },
+  {
+    "revision": "bfc0faa23284aba4cf0b",
+    "url": "/Todo-App/static/js/2.62e3cc26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-App/static/js/2.62e3cc26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe9f2cdeecee508c4fd2",
+    "url": "/Todo-App/static/js/main.969d0b25.chunk.js"
+  },
+  {
+    "revision": "11e6e82e1057fd23d378",
+    "url": "/Todo-App/static/js/runtime-main.b98ae28a.js"
+  }
+]);
